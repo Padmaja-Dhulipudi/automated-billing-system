102dffb7cd5e759aa5488fd4d7243d2cab55d761
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-07-12 22:25:01 +0530

    Import Bootstrap CSS in main.jsx
    Added Bootstrap CSS import for styling.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
