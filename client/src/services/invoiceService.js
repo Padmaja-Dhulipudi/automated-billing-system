cb16b41860608c3fb5b5cbbf8695ca4da285491b
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-07-16 19:01:45 +0530

    Update invoiceService.js

--- client/src/services/invoiceService.js
+++ client/src/services/invoiceService.js
@@ -1,1 +1,26 @@
+import api from "./api";
 
+// Get All Invoices
+export const getInvoices = () => {
+  return api.get("/invoices");
+};
+
+// Get Single Invoice
+export const getInvoiceById = (id) => {
+  return api.get(`/invoices/${id}`);
+};
+
+// Create Invoice
+export const createInvoice = (invoice) => {
+  return api.post("/invoices", invoice);
+};
+
+// Update Invoice
+export const updateInvoice = (id, invoice) => {
+  return api.put(`/invoices/${id}`, invoice);
+};
+
+// Delete Invoice
+export const deleteInvoice = (id) => {
+  return api.delete(`/invoices/${id}`);
+};
