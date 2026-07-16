d8b52697e3ac2660db4876bb50347ecc768c7340
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-07-16 18:53:14 +0530

    Add Navbar component with routing links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,1 +1,44 @@
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+      <div className="container-fluid">
+
+        <Link className="navbar-brand fw-bold" to="/dashboard">
+          Automated Billing
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">
+                Dashboard
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Logout
+              </Link>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
