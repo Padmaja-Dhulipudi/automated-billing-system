b9d7463a3b3f974369185628b67f63ad6fefb76c
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-07-16 19:01:26 +0530

    Update productService.js

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,1 +1,26 @@
+import api from "./api";
 
+// Get All Products
+export const getProducts = () => {
+  return api.get("/products");
+};
+
+// Get Single Product
+export const getProductById = (id) => {
+  return api.get(`/products/${id}`);
+};
+
+// Add Product
+export const addProduct = (product) => {
+  return api.post("/products", product);
+};
+
+// Update Product
+export const updateProduct = (id, product) => {
+  return api.put(`/products/${id}`, product);
+};
+
+// Delete Product
+export const deleteProduct = (id) => {
+  return api.delete(`/products/${id}`);
+};
