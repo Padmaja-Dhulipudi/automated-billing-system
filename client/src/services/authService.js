62811cf2ef58ce735e93a50b268135440a907f02
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-07-16 19:00:44 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,1 +1,16 @@
+import api from "./api";
 
+// Register User
+export const registerUser = (userData) => {
+  return api.post("/auth/register", userData);
+};
+
+// Login User
+export const loginUser = (userData) => {
+  return api.post("/auth/login", userData);
+};
+
+// Get Logged-in User
+export const getProfile = () => {
+  return api.get("/auth/profile");
+};
