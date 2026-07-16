3bd8a0baf1f4c37b7517c90d74eec3dbfe3cb58a
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-07-16 18:52:11 +0530

    Update NotFound.jsx

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,1 +1,20 @@
+import { Link } from "react-router-dom";
 
+function NotFound() {
+  return (
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <h1 className="display-1">404</h1>
+
+      <h3>Page Not Found</h3>
+
+      <Link to="/" className="btn btn-primary mt-3">
+        Back to Login
+      </Link>
+    </div>
+  );
+}
+
+export default NotFound;
