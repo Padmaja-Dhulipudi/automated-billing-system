7f2804758fe08a1650cfeb104c10366c9a8aaff5
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-07-16 19:01:04 +0530

    Update customerService.js

--- client/src/services/customerService.js
+++ client/src/services/customerService.js
@@ -1,1 +1,26 @@
+import api from "./api";
 
+// Get All Customers
+export const getCustomers = () => {
+  return api.get("/customers");
+};
+
+// Get Single Customer
+export const getCustomerById = (id) => {
+  return api.get(`/customers/${id}`);
+};
+
+// Add Customer
+export const addCustomer = (customer) => {
+  return api.post("/customers", customer);
+};
+
+// Update Customer
+export const updateCustomer = (id, customer) => {
+  return api.put(`/customers/${id}`, customer);
+};
+
+// Delete Customer
+export const deleteCustomer = (id) => {
+  return api.delete(`/customers/${id}`);
+};
